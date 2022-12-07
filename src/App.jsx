a0111641dec5a9a8ebee0ea9c009b28994c8fedd
rafelis1997 <rafaelheros80@gmail.com>
2022-12-07 08:34:36 -0300

    - Dracaris

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,23 @@ import 'mind-ar/dist/mindar-image.prod.js';
 import 'aframe';
 import 'mind-ar/dist/mindar-image-aframe.prod.js';
 import 'aframe-extras'
+import './aframe-particle-system-component.min.js';
 
 import logoSvg from './assets/logo.svg';
 import deathHuntCover from './assets/deathHuntCover.png';
+import fireIcon from './assets/fireIcon.png';
 import './App.css';
 
 function App() {
   const sceneRef = useRef(null);
   const scanUi = useRef(null);
+  const particleFire = useRef(null);
+
+  function handleSetFire() {
+    const fireParticles = particleFire.current.components['particle-system'];
+    fireParticles.stopParticles();
+    fireParticles.startParticles();
+  }
 
   useEffect(() => {
     const sceneEl = sceneRef.current;
@@ -60,6 +69,10 @@ function App() {
           </div>
         </div>
 
+        <button onClick={handleSetFire} className="fireButton">
+          <img src={fireIcon} alt="" width={50} height={50}/>
+        </button>
+
         <a-scene
           ref={sceneRef}
           mindar-image={`uiScanning: #scanUi;imageTargetSrc: targets.mind;filterMinCF:0.0001; filterBeta: 0.0001`} 
@@ -71,7 +84,9 @@ function App() {
         >
           <a-camera position="0 0 0" look-controls="enabled: false"></a-camera>
           <a-entity mindar-image-target="targetIndex: 0">
-            <a-gltf-model animation-mixer="clip: dragonAnim" src="dragon.glb" rotation="0 0 0" position="0 -1.5 0.2" scale="0.5 0.5 0.5" animation="property: rotation; to: 0 360 0; dur: 25000; easing: easeInOutQuad; loop: true; dir: alternate"></a-gltf-model>
+            <a-gltf-model animation-mixer="clip: dragonAnim" src="dragon.glb" rotation="0 90 0" position="-0.5 -1.5 0.2" scale="0.35 0.35 0.35">
+            <a-entity ref={particleFire} position="-0.007 4.409 3.579" rotation="128.569 7.206 0.656" particle-system="maxAge: 3;color: #ef6c00,#ccbe00;blending: 1;size:150; texture: fire_texture.png; enabled: false; duration: 2" material="alphaTest: 0.5"></a-entity>
+            </a-gltf-model>
           </a-entity>
         </a-scene>
       </div>
@@ -80,3 +95,4 @@ function App() {
 }
 
 export default App
+
